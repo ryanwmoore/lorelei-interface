4a6a9b5c698e005a6576e7d22ad0b30a28894e96
Ryan W. Moore <rwmoore07@gmail.com>
2015-12-17 20:17:57 -0500

    Move player ID editing to /users/
    Reset player ID text box after adding player ID.

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/user/', user);
+app.use('/players/', user);
 app.use('/tournaments/', tournaments);
 
 // catch 404 and forward to error handler
