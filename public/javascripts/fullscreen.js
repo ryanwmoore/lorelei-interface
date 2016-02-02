e6b130f26f83702c1ea8d80a32a8b39e5bd191d3
Ryan W. Moore <rwmoore07@gmail.com>
2016-02-01 21:47:30 -0500

    Fullscreen button

---
+++ public/javascripts/fullscreen.js
@@ -0,0 +1,32 @@
+//This file's contents from https://xparkmedia.com/blog/enter-fullscreen-mode-javascript/
+
+function toggleFullScreen() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
+      (!document.mozFullScreen && !document.webkitIsFullScreen)) {               // current working methods
+    if (document.documentElement.requestFullScreen) {
+      document.documentElement.requestFullScreen();
+    } else if (document.documentElement.mozRequestFullScreen) {
+      document.documentElement.mozRequestFullScreen();
+    } else if (document.documentElement.webkitRequestFullScreen) {
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+    }
+  } else {
+    if (document.cancelFullScreen) {
+      document.cancelFullScreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitCancelFullScreen) {
+      document.webkitCancelFullScreen();
+    }
+  }
+}
+
+function cancelFullScreen() {
+   if (document.cancelFullScreen) {
+     document.cancelFullScreen();
+   } else if (document.mozCancelFullScreen) {
+     document.mozCancelFullScreen();
+   } else if (document.webkitCancelFullScreen) {
+     document.webkitCancelFullScreen();
+   }
+}
